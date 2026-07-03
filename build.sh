9471fe2775e1cd6cbe918899f8e278dbec2632f2
Kartatz <105828205+Kartatz@users.noreply.github.com>
2026-07-03 18:51:25 -0300

    Rename __unused to __unused0

--- build.sh
+++ build.sh
@@ -17,6 +17,10 @@ patch --directory="${PWD}/libc-shim" --strip='1' --input="${workdir}/patches/001
 
 ${apt} install -y libpng-dev libevdev-dev libudev-dev libx11-dev libegl1-mesa-dev libxi-dev libxcursor-dev libxrandr-dev libxss-dev libgl-dev libssl-dev
 
+cmake -S . -B build -DBUILD_UI=OFF   -DBUILD_WEBVIEW=OFF   -DBUILD_CLIENT=ON   -DUSE_OWN_CURL=ON -DCMAKE_BUILD_TYPE='Release' -DCMAKE_INSTALL_PREFIX=/tmp/minecraft-linux
+make -C build
+make -C build install
+
 git clone https://github.com/LNSSPsd/arm64-mcpelauncher-server --recursive
 cd arm64-mcpelauncher-server
 
