6d3685b01bfe3bcabbe191da7af12f1e1747d37a
Kartatz <105828205+Kartatz@users.noreply.github.com>
2026-07-02 23:54:20 -0300

    https://github.com/Kartatz/ll

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -eu
+
 declare -r workdir="${PWD}"
 
 declare -r apt="${CC/clang/apt}"
@@ -7,7 +9,7 @@ declare -r apt="${CC/clang/apt}"
 git clone https://github.com/minecraft-linux/mcpelauncher-manifest
 cd mcpelauncher-manifest
 
-$workdir/patches/apply_patches.sh
+bash $workdir/patches/apply_patches.sh
 
 ${apt} install -y libpng-dev libevdev-dev libudev-dev libx11-dev libegl1-mesa-dev libxi-dev libxcursor-dev libxrandr-dev libxss-dev libgl-dev libssl-dev
 
